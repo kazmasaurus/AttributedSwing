1d4ec1e48b1330cc2968f0452a5aa99c6dedf4c9
Zak Remer <zak.remer@gmail.com>
2015-09-01 01:49:53 -0700

    Adds empty xcode project.

---
+++ AttributedSwing/AttributedSwing.h
@@ -0,0 +1,19 @@
+//
+//  AttributedSwing.h
+//  AttributedSwing
+//
+//  Created by Zak Remer on 9/1/15.
+//  Copyright © 2015 Zak Remer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AttributedSwing.
+FOUNDATION_EXPORT double AttributedSwingVersionNumber;
+
+//! Project version string for AttributedSwing.
+FOUNDATION_EXPORT const unsigned char AttributedSwingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AttributedSwing/PublicHeader.h>
+
+
